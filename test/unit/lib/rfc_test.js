8594b099cef68703ec08f97c68c8b3a75823898e
Paul Jensen <paulbjensen@gmail.com>
2012-10-28 15:25:31 +0000

    added unfleshed tests for rfc.js

---
+++ test/unit/lib/rfc_test.js
@@ -0,0 +1,20 @@
+var assert = require('assert')
+  , rfc    = require('../../../lib/rfc');
+
+describe("rfc", function(){
+
+  describe("#addCommand", function(){
+
+    it("should store a command locally for future reference");
+
+  });
+
+  describe("#executeCommand", function(){
+
+    it("should call a stored command");
+
+    it("should make the socket send the result of the command's callback");
+
+  });
+
+});
