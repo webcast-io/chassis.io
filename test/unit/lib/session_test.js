a0a0ba7ecd45a437399d8576b35ba0993bb880da
Paul Jensen <paulbjensen@gmail.com>
2013-02-19 06:12:23 +0000

    made an in-memory module for session-sockets

--- test/unit/lib/session_test.js
+++ test/unit/lib/session_test.js
@@ -4,12 +4,32 @@ var assert  = require('assert')
 
 describe("Session", function(){
 
+  var sessionId = "d29h928dh29";
+  var socketId  = "AAA8hdh92hd";
+
+  beforeEach(function(done){
+    session.getSocketsForSession(sessionId, function(err,sockets){
+
+      if (sockets.length == 0) {
+        done();
+      } else {
+        var length = sockets.length;
+        for (var i =0; i<length;i++) {
+          session.removeSocketFromSession(sockets[i],sessionId, function(err,response){
+            if (i==length-1){
+              done();
+            };
+          });
+        }
+      }
+
+    });
+  });
+
   describe("#addSocketToSession", function(){
 
     // Q - where, and how do we store this data?
     it("should add the socket id to the session's list of sockets", function(done){
-      var sessionId = "d29h928dh29";
-      var socketId  = "AAA8hdh92hd";
       session.addSocketToSession(socketId,sessionId, function(err,response){
         assert.equal(true, response);
         session.getSocketsForSession(sessionId,function(err,sockets){
@@ -26,18 +46,16 @@ describe("Session", function(){
   describe("#removeSocketFromSession", function(){
 
     it("should remove the socket id from the session's list of sockets", function(done){
-      var sessionId = "d29h928dh29";
-      var socketId  = "AAA8hdh92hd";      
       session.addSocketToSession(socketId,sessionId, function(err,response){
         assert.equal(null, err);
         assert.equal(true, response);
-        session.removeSocketFromSession(socketId, sessionId, function(err, response2){
+        session.removeSocketFromSession(socketId, sessionId, function(err2, response2){
           assert.equal(null, err2);
           assert.equal(true, response2);
 
           session.getSocketsForSession(sessionId,function(err,sockets){
             assert.equal(-1, sockets.indexOf(socketId));
-            assert.equal([], sockets);
+            assert.equal([].length, sockets.length);
             done();
           });
 
@@ -50,8 +68,6 @@ describe("Session", function(){
   describe("#getSocketsForSession", function(){
 
     it("should return the list of socket ids for the session", function(done){
-      var sessionId = "d29h928dh29";
-      var socketId  = "AAA8hdh92hd";
       session.addSocketToSession(socketId,sessionId, function(err,response){
         assert.equal(true, response);
         session.getSocketsForSession(sessionId,function(err,sockets){
